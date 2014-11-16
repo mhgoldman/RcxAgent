c19e0edcef1b2428583f9a47f83072b701b1add2
Martin Goldman <martin@mgoldman.com>
2014-11-16 17:55:10 -0500

    minor cleanup

--- RcxAgent/RcxService.cs
+++ RcxAgent/RcxService.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
 using System.ServiceModel;
-using System.Text;
 using System.IO;
 using System.ServiceModel.Web;
 using System.Net;
